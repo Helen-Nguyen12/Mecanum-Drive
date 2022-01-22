c297b9d60077e2137338d247e7b3c317b3753649
Isaiah-Milkey <isaiah.milkey@pridetronics.com>
2022-01-22 11:41:34 -0700

    Test

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,10 +4,18 @@
 
 package frc.robot;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.PowerDistribution;
+import edu.wpi.first.wpilibj2.command.button.*;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 
 /** This is a demo program showing how to use Mecanum control with the MecanumDrive class. */
 public class Robot extends TimedRobot {
@@ -15,19 +23,24 @@ public class Robot extends TimedRobot {
   private static final int kRearLeftChannel = 2;
   private static final int kFrontRightChannel = 1;
   private static final int kRearRightChannel = 3;
-
   private static final int kJoystickChannel = 0;
 
   private MecanumDrive m_robotDrive;
-  private Joystick m_stick;
+  private Joystick m_stick = new Joystick(kJoystickChannel);
+  
+  private JoystickButton flyWheelRun;
+
+  // private final PowerDistribution PDP = new PowerDistribution();
+
+  VictorSP frontLeft = new VictorSP(kFrontLeftChannel);
+  VictorSP rearLeft = new VictorSP(kRearLeftChannel);
+  VictorSP frontRight = new VictorSP(kFrontRightChannel);
+  VictorSP rearRight = new VictorSP(kRearRightChannel);
+
+   CANSparkMax flyWheel = new CANSparkMax(4, MotorType.kBrushless);
 
   @Override
   public void robotInit() {
-    VictorSP frontLeft = new VictorSP(kFrontLeftChannel);
-    VictorSP rearLeft = new VictorSP(kRearLeftChannel);
-    VictorSP frontRight = new VictorSP(kFrontRightChannel);
-    VictorSP rearRight = new VictorSP(kRearRightChannel);
-
     // Invert the right side motors.
     // You may need to change or remove this to match your robot.
     frontRight.setInverted(true);
@@ -35,13 +48,28 @@ public class Robot extends TimedRobot {
 
     m_robotDrive = new MecanumDrive(frontLeft, rearLeft, frontRight, rearRight);
 
-    m_stick = new Joystick(kJoystickChannel);
+    m_stick =  new Joystick(kJoystickChannel);
+
   }
 
   @Override
   public void teleopPeriodic() {
     // Use the joystick X axis for lateral movement, Y axis for forward
     // movement, and Z axis for rotation.
-    m_robotDrive.driveCartesian(m_stick.getX(), m_stick.getY(), m_stick.getZ(), 0.0);
+
+    SmartDashboard.getNumber("Right Motor", frontLeft.get());
+    
+    m_robotDrive.driveCartesian(m_stick.getX() * 0.1, m_stick.getY() * 0.1, m_stick.getZ() * 0.1, 0.0);
+
+   if (m_stick.getRawButtonPressed(4)){
+    flyWheel.set(1);
+   } 
+   else {
+     flyWheel.set(0);
+   }
+
+    
   }
+
+ 
 }
